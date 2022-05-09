3f091a0098fcd7002e789638b07a6312ee41e481
toryoto <iniad@DESKTOP-6KCGF5Q.localdomain>
2022-05-09 14:37:41 +0900

    first commit

---
+++ front/src/components/CalendarIndex.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <v-sheet height="6vh" class="d-flex align-center">
+      <v-btn outlined small class="ma-4" @click="setToday">今日</v-btn>
+
+      <v-btn icon @click="$refs.calendar.prev()">
+        <v-icon>mdi-chevron-left</v-icon>
+      </v-btn>
+
+      <v-btn icon @click="$refs.calendar.next()">
+        <v-icon>mdi-chevron-right</v-icon>
+      </v-btn>
+
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
+    </v-sheet>
+
+    <v-sheet height="94vh">
+      <v-calendar
+        ref="calendar"
+        v-model="value"
+        :events="events"
+        @change="fetchEvents"
+        locale="ja-jp"
+        :day-format="(timestamp) => new Date(timestamp.date).getDate()"
+        :month-format="
+          (timestamp) => new Date(timestamp.date).getMonth() + 1 + ' /'
+        "
+      ></v-calendar>
+    </v-sheet>
+  </div>
+</template>
+
+<script>
+import { format } from "date-fns"; // 日付を表示用の文字列に変換
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  name: "CalendarIndex",
+
+  data: () => ({
+    value: format(new Date(), "yyyy/MM/dd"),
+  }),
+
+  computed: {
+    ...mapGetters("events", ["events"]),
+    title() {
+      //return format(this.value, 'yyyy年 M月');
+      return format(new Date(this.value), "yyyy年 M月");
+    },
+  },
+
+  methods: {
+    ...mapActions("events", ["fetchEvents"]),
+    setToday() {
+      this.value = format(new Date(), "yyyy/MM/dd");
+    },
+  },
+};
+</script>
---
+++ front/src/store/modules/events.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+const apiUrl = "http://localhost:3000";
+
+const state = {
+  events: [],
+};
+
+const getters = {
+  events: (state) =>
+    state.events.map((event) => {
+      return {
+        ...event, // スプレッド構文
+        start: new Date(event.start),
+        end: new Date(event.end),
+      };
+    }),
+};
+
+const mutations = {
+  setEvents: (state, events) => (state.events = events),
+};
+
+const actions = {
+  async fetchEvents({ commit }) {
+    const response = await axios.get(`${apiUrl}/events`);
+    commit("setEvents", response.data); // mutationを呼び出す
+  },
+};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  mutations,
+  actions,
+};
